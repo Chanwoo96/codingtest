b557e991c7e553de8dd7201a5e870e216199f33b
kcwoo <kcwoo7777@naver.com>
2022-12-09 21:10:00 +0900

    불필요한 import 수정

--- codingtest01/src/practice1207/홀짝구분.java
+++ codingtest01/src/practice1207/홀짝구분.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class 홀짝구분 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String result="Odd";
-		if(sc.nextInt()%2==0) {
-			result="Even";
+		try (Scanner sc = new Scanner(System.in)) {
+			String result="Odd";
+			if(sc.nextInt()%2==0) {
+				result="Even";
+			}
+			System.out.println(result);
 		}
-		System.out.println(result);
 	}
 
 }
